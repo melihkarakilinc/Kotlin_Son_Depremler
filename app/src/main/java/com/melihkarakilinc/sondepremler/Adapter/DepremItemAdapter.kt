8df28713a1b27cafca4b7268ab6e24868fab2d88
melihkarakilinc <57638333+melihkarakilinc@users.noreply.github.com>
2021-12-04 22:39:08 +0300

    #Create Google Map API in mapView

--- app/src/main/java/com/melihkarakilinc/sondepremler/Adapter/DepremItemAdapter.kt
+++ app/src/main/java/com/melihkarakilinc/sondepremler/Adapter/DepremItemAdapter.kt
@@ -1,16 +1,27 @@
 package com.melihkarakilinc.sondepremler.Adapter
 
+
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.melihkarakilinc.sondepremler.Model.DepremInf
 import com.melihkarakilinc.sondepremler.databinding.DepremItemBinding
+
+
+
+
 
 class DepremItemAdapter() :
     RecyclerView.Adapter<DepremItemAdapter.ViewHolder>() {
 
     var itemList=mutableListOf<DepremInf>()
+    lateinit var onClickListener: View.OnClickListener
 
     @SuppressLint("NotifyDataSetChanged")
     fun depremFunList(itemList: List<DepremInf>) {
@@ -27,9 +38,20 @@ class DepremItemAdapter() :
     override fun getItemCount(): Int = itemList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.txtSehir.text=itemList[position].yer
-        holder.binding.txtSiddet.text=itemList[position].siddet
-        holder.binding.txtTarih.text=itemList[position].tarih
+        holder.binding.txtSehir.text = itemList[position].yer
+        holder.binding.txtSiddet.text = itemList[position].siddet
+        holder.binding.txtTarih.text = itemList[position].tarih
+        holder.binding.mapView.onCreate(null)
+        holder.binding.mapView.getMapAsync(OnMapReadyCallback { googleMap ->
+            var mMap = googleMap
+            mMap.getUiSettings().setZoomControlsEnabled(true)
+            val lat1 = itemList[position].enlem.toDouble()
+            val lng1 = itemList[position].boylam.toDouble()
+            val bord = LatLng(lat1, lng1)
+            mMap.addMarker(MarkerOptions().position(bord).title(itemList[position].yer))
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(bord, 8F))
+        })
+        holder.binding.mapView.postInvalidate()
 
     }
 
